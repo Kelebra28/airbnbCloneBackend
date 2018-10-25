ad57a446edb991fe4f1469dd9fcbd119c743932b
Kelebra <ricardokelebra@gmail.com>
2018-10-24 19:26:36 -0500

    test

---
+++ server/test/setUp.js
@@ -0,0 +1,25 @@
+const {Users,Houses,Bookings,Addresses,Facilities,Comments} = require('../models');
+
+
+module.exports = () => {
+    Users.truncate({
+         cascade:true
+    })
+    Houses.truncate({
+        cascade:true
+    })
+    Bookings.truncate({
+        cascade:true
+   })
+
+    Addresses.truncate({
+        cascade:true
+   })
+    Facilities.truncate({
+        cascade:true
+   })
+    Comments.truncate({
+        cascade:true
+   })
+
+}
